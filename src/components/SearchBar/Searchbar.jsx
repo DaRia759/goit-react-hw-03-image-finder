e1babf7f4deed6820c59c9c85b4000f61ce673cd
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-08-22 19:46:08 +0200

    Update Searchbar.jsx

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import css from './SearchBar.module.css';
